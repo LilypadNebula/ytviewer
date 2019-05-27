60f264620dfd526423f5d59923d3fb98655d3bc5
Lily <lilypadnebula@gmail.com>
2019-05-26 22:51:43 -0400

    Base commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,124 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="flex flex-col items-center p-4 container mx-auto">
+    <div class="flex flex-col md:flex-row mb-4">
+      <div class="flex items-center">
+        <font-awesome-icon
+          icon="search"
+          class="text-grey-500 absolute ml-6"
+        ></font-awesome-icon>
+        <input
+          type="text"
+          class="text-3xl rounded-full text-center p-2 m-2"
+          v-model="search"
+        />
+      </div>
+      <button
+        class="text-xl rounded-full px-4 m-2 bg-purple-500 text-white"
+        @click="sendSearch"
+      >
+        Search
+      </button>
+    </div>
+    <div
+      class="flex justify-center items-center w-2/3"
+      v-if="videos.length > 0"
+    >
+      <button @click="decrementVid">
+        <font-awesome-icon
+          icon="caret-left"
+          size="3x"
+          class="m-2 text-purple-500"
+        ></font-awesome-icon>
+      </button>
+      <youtube
+        :video-id="selectedVideo.id"
+        ref="youtube"
+        :player-vars="playerVars"
+        @ready="videoReady"
+      ></youtube>
+      <button @click="incrementVid">
+        <font-awesome-icon
+          icon="caret-right"
+          size="3x"
+          class="m-2 text-purple-500"
+        ></font-awesome-icon>
+      </button>
+    </div>
+    <p class="text-xl md:text-3xl text-center">
+      YouTube Viewer, a quick way to search for videos without all the
+      distractions
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { google } from "googleapis";
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: process.env.VUE_APP_KEY
+});
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      videos: [],
+      selectedIndex: 0,
+      search: "",
+      loading: true,
+      playerVars: {
+        origin: this.windowLoc
+      }
+    };
+  },
+  methods: {
+    async sendSearch() {
+      const results = await youtube.search.list({
+        part: "snippet,contentDetails,statistics",
+        q: this.search
+      });
+      // this.videos = results.data.items;
+      console.log(results);
+    },
+    videoReady() {
+      this.loading = false;
+      console.log("Video done");
+    },
+    decrementVid() {
+      if (this.selectedIndex === 0) this.selectedIndex = this.videos.length - 1;
+      else this.selectedIndex = this.selectedIndex - 1;
+    },
+    incrementVid() {
+      if (this.selectedIndex === this.videos.length - 1) this.selectedIndex = 0;
+      else this.selectedIndex = this.selectedIndex + 1;
+    }
+  },
+  async mounted() {
+    const temp = await youtube.videos.list({
+      part: "snippet,contentDetails,statistics",
+      id: "0LHxvxdRnYc,pU8-7BX9uxs,bILE5BEyhdo,jCyEX6u-Yhs,adzYW5DZoWs"
+    });
+    this.videos = temp.data.items;
+    this.videoIndex = 0;
+  },
+  computed: {
+    selectedVideo() {
+      return this.videos[this.selectedIndex];
+    },
+    player() {
+      return this.$refs.youtube.player;
+    }
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  background-color: hsl(0, 0%, 90%);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueYoutube from "vue-youtube";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faSearch,
+  faCaretLeft,
+  faCaretRight
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSearch, faCaretLeft, faCaretRight);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.use(VueYoutube);
+
+Vue.prototype.windowLoc = window.location.href;
 
 Vue.config.productionTip = false;
 
